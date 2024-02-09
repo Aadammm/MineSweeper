35b82760e0b94a2b78313d4aa8e4089e757ea345
janak <janak@ADAMOVINA>
2024-02-09 15:40:45 +0100

    Add project files.

---
+++ Blbosti/Area.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blbosti
+{
+    internal class Area
+    {
+
+        Random random;
+        public readonly int[,] field;
+        public int mine = -1;
+        public int this[int row, int column]
+        {
+            get { return field[row, column]; }
+        }
+
+        public Area(int width, int height, int countMines)
+        {
+            random = new Random();
+            field = new int[width, height];
+            FillField(field.GetLength(0), field.GetLength(1), countMines);
+        }
+        private void FillField(int width, int height, int mines)
+        {
+            int x = random.Next(width);
+            int y = random.Next(height);
+            //  fill field with mines
+            for (int z = 0; z < mines; z++)
+            {
+                while (field[x, y] == mine)
+                {
+                    x = random.Next(width);
+                    y = random.Next(height);
+                }
+                field[x, y] = mine;
+                for (int i = Math.Max(x - 1, 0); i <= Math.Min(x + 1, width - 1); i++)
+                    for (int j = Math.Max(y - 1, 0); j <= Math.Min(y + 1, height - 1); j++)
+                        if (field[i, j] != mine)
+                            field[i, j]++;
+            }
+        }
+    }
+}
---
+++ Blbosti/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Globalization;
+using System.Threading;
+using System.Windows.Forms;
+namespace Blbosti
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string sizeText = "what size will the field be?: ";
+            string minesText = "number of mines?: ";
+            int size; int mines;
+            Console.Write(sizeText);
+            while (!int.TryParse(Console.ReadLine(), out size)||size<=0||size>30)
+            {
+                Console.Clear();
+                Console.Write(sizeText);
+            }
+            Console.Write(minesText);
+            while (!int.TryParse(Console.ReadLine(), out mines)||(size*size)<mines)
+            {
+                Console.Clear();
+                Console.Write(minesText);
+            }
+            Game game = new Game(size, mines);
+            game.GameProcess();
+            Console.Read();
+            }
+            
+        }
+
+
+
+    }
+}
